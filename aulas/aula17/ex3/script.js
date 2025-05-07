87435e71c844cc6b45a5f28cd4747bc44286e92d
pedro <pedrohenripinter0@gmail.com>
2025-05-07 14:18:32 -0300

    pnc do carioca

--- aulas/aula17/ex3/script.js
+++ aulas/aula17/ex3/script.js
@@ -8,11 +8,11 @@ fetch("./data.json")
 .then(response => response.json())
 .then(data =>{
 
-for (let i = 0; i < data.pessoas.length; i++) {
+
 nome = data.pessoas[i].nome;
 idade = data.pessoas[i].idade;
 cpf = data.pessoas[i].cpf;
 telefone = data.pessoas[i].telefone;
 
     document.getElementById("demo").innerHTML = "Usuario: "+nome+"\nIdade: "+idade+"\nCPF: "+cpf+"\nTelefone: "+telefone  
-}})
+})
